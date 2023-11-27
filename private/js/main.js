6bebe0c831e050975acba5481e21c7b80b815b13
Khaja Kutub Uddin <ridmikkutub@gmail.com>
2023-11-27 23:59:23 +1030

    added comment

--- private/js/main.js
+++ private/js/main.js
@@ -2,7 +2,7 @@ var user;
 
 function escapeHTML(str) {
     return String(str)
-        .replace(/&/g, '&amp;')
+        .replace(/&/g, '&amp;') //xss santization
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
         .replace(/"/g, '&quot;')
